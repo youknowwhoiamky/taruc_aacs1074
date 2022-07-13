db7e80453999751e23ee822e61858beb44aac436
youknowwhoiamky <sosleong365@gmail.com>
2022-07-13 11:35:53 +0800

    Update Practical_02

--- Practical_02/Practical 02/Question_03.c
+++ Practical_02/Practical 02/Question_03.c
@@ -7,19 +7,27 @@ main() {
 	int heightCM;
 	double heightIN;
 
+	int ageInYears, ageInDays;
+
 	printf("Enter your first name: ");
 	gets(firstName);
 
 	printf("Enter your last name: ");
 	gets(lastName);
 
-	printf("Enter your height in \"cm\": ");
+	printf("Enter your age in \"years\"");
+	scanf("%d", &ageInYears);
+
+	printf("Enter your height in \"cm\": "); 
 	scanf("%d", &heightCM);
 
 	heightIN = heightCM / 2.54;
 
+	ageInDays = ageInYears * 365;
+
 	printf("%s %s\n", firstName, lastName);
 	printf("Your height is %f inches.\n", heightIN);
+	printf("Your age is %d in days!!!.\n", ageInDays);
 
 	system("pause");
 }
